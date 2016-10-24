238d032c2752470a4c11cfe812f3a4a2924b4cb9
MTibby92 <MTibby92@gmail.com>
2016-10-24 15:10:31 -0500

    uncommitted changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
+var htmlRoutes = require('./app/routing/html-routes.js')
+var apiRoutes = require('./app/routing/api-routes.js')
 
 // Sets up the Express App
 // =============================================================
